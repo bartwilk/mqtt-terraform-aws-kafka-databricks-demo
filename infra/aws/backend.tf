bb6159cf167d584fe5366a5bf8fbf6b15153845f
bartwilk <bartwilk@gmail.com>
2026-04-02 00:19:51 -0400

    Re-enable public EKS endpoint, add kubernetes provider declaration

--- infra/aws/backend.tf
+++ infra/aws/backend.tf
@@ -18,5 +18,9 @@ terraform {
       source  = "hashicorp/helm"
       version = "~> 2.12"
     }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.27"
+    }
   }
 }
